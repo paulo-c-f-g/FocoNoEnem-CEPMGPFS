a3e30f7a3bdb556a2e27ce4f4ebc1185d4669952
paulo-c-f-g <codingpaulocesarfernandesgama@gmail.com>
2025-08-25 19:46:28 -0300

    adding page header

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const headerNav = document.getElementById("header-navigation");
+const headerMenuBtn = document.getElementById("header-menu-btn")
+
+headerMenuBtn.addEventListener( 'click', () => {
+    headerNav.classList.toggle("header__nav--active");
+})
