8522f19bc9a9b667ffb5ac068cd8ea51e734e901
paulo-c-f-g <codingpaulocesarfernandesgama@gmail.com>
2025-08-25 21:55:21 -0300

    Adding a close icon to the menu button

--- js/app.js
+++ js/app.js
@@ -3,4 +3,5 @@ const headerMenuBtn = document.getElementById("header-menu-btn")
 
 headerMenuBtn.addEventListener( 'click', () => {
     headerNav.classList.toggle("header__nav--active");
+    headerMenuBtn.classList.toggle("header__menu-btn--active");
 });
